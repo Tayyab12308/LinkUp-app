64aeb687a3645f79230f276aa09df21343934300
Tayyab Iqbal <tayyab12308@gmail.com>
2019-03-31 18:34:01 +0000

    Make a private conversation windows new message form functional add event handle inside conversation.js channels to trigger submit button define common behavior of conversation windows in javascripts/conversations/conversation.js define a send_message function on client and server sides

--- app/assets/javascripts/channels/private/conversation.js
+++ app/assets/javascripts/channels/private/conversation.js
@@ -1,5 +1,17 @@
 App.private_conversation = App.cable.subscriptions.create("Private::ConversationChannel", {
   connected: function() {},
   disconnected: function() {},
-  received: function() {}
+  received: function() {},
+  send_message: function() {
+    return this.perform('send_message', {
+      message: message
+    });
+  }
+});
+
+$(document).on('submit', '.send-private-message', function(e) {
+  e.preventDefault();
+  var values = $(this).serializeArray();
+  App.private_conversation.send_message(values);
+  $(this).trigger('reset');
 });
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,13 +11,12 @@ class ApplicationController < ActionController::Base
   end
   
   def opened_conversations_windows
-    if (user_signed_in? && @private_conversations)
-      session[:private_conversations] ||= []
-      @private_conversations_windows = Private::Conversation.includes(:recipient, :messages)
-                                        .find(session[:private_conversations])
-    else
-      @private_conversations_windows = []
-    end
+  if user_signed_in? && session[:private_conversations].present?
+    @private_conversations_windows = Private::Conversation.includes(:recipient, :messages)
+                                        .where(id: session[:private_conversations])
+  else
+    @private_conversations_windows = []
   end
+end
   
 end
