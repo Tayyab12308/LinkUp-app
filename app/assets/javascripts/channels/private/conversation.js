a9edc62fc8deb21d6f7a46030b2209dfead37368
Tayyab Iqbal <tayyab12308@gmail.com>
2019-03-31 19:54:25 +0000

    broadcast a new message Define an after_create_commit callback method in Private::Message create a Private::MessageBroadcastJob define a received function inside the javascripts/channels/private/conversation.js careate a conversation.js inside the assets/javascripts/channels/shared

--- app/assets/javascripts/channels/private/conversation.js
+++ app/assets/javascripts/channels/private/conversation.js
@@ -1,7 +1,34 @@
 App.private_conversation = App.cable.subscriptions.create("Private::ConversationChannel", {
   connected: function() {},
   disconnected: function() {},
-  received: function() {},
+  received: function(data) {
+    var conversation_menu_link = $('#conversations-menu u1').find('#menu-pc' + data['conversation_id']);
+    if(conversation_menu_link.length) {
+      conversation_menu_link.prependTo('#conversations-menu u1');
+    }
+    var conversation = findConv(data['conversation_id'], 'p');
+    var conversation_rendered = ConvRendered(data['conversation_id'], 'p');
+    var messages_visible = ConvMessagesVisibility(conversation);
+    
+    if(data['recipient'] == true) {
+      $('#menu-pc' + data['conversation_id']).addClass('unseen-conv');
+      if(conversation_rendered) {
+        if(!messages_visible) {
+          
+        }
+        conversation.find('.messages-list').find('u1').append(data['message']);
+      }
+      calculateUnseenConversations();
+    }
+    else {
+      conversation.find('u1').append(data['message']);
+    }
+    if (conversation.length) {
+      var messages_list = conversation.find('.messages-list');
+      var height = messages_list[0].scrollHeight;
+      messages_list.scrollTop(height);
+    }
+  },
   send_message: function() {
     return this.perform('send_message', {
       message: message
--- app/controllers/concerns/messages.rb
+++ app/controllers/concerns/messages.rb
@@ -6,7 +6,7 @@ module Messages
   def get_messages(conversation_type, messages_amount)
     model = "#{conversation_type.capitalize}::Conversation".constantize
     @conversation = model.find(params[:conversation_id])
-    @messages - @conversation.messaes.order(created_at: :desc)
+    @messages - @conversation.messages.order(created_at: :desc)
                                      .limit(messages_amount)
                                      .offset(params[:messages_to_display_offset].to_i)
     @messages_to_display_offset = params[:messages_to_display_offset].to_i + messages_amount
