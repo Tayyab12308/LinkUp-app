fa05a0ee19c0e8f658d05aa63f275ed69cfed004
Tayyab Iqbal <tayyab12308@gmail.com>
2019-04-03 20:19:12 +0000

    create a ContactsController and define routes to its actions

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
     end
   end
   
+  resources :contacts, only: [:create, :update, :destroy]
+  
   namespace :private do
     resources :conversations, only: [:create] do
       member do
